6387468e12b5543b22be2dfe7cf5dafc2cb8ccb8
TH-Raju <rajukhan894200@gmail.com>
2022-11-07 21:49:59 +0600

    project setup done

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='text-red-600'>
+          Edit <code>src/App.js</code>
         </p>
         <a
           className="App-link"
