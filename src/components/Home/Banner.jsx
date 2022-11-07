16d3012a855758f5995f6214c3134c1331878d48
TH-Raju <rajukhan894200@gmail.com>
2022-11-07 22:52:17 +0600

    Footer section setup

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -2,20 +2,15 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='text-white'>
+        <div className='text-white bg-gray-400'>
             <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
                 <p className='text-[#00df9a] font-bold p-2'>
-                    GROWING WITH DATA ANALYTICS
+                    GROWING WITH LEARNING
                 </p>
                 <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
-                    Grow with data.
+                    Grow with tech.
                 </h1>
-                <div className='flex justify-center items-center'>
-                    <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-                        Fast, flexible financing for
-                    </p>
-                </div>
-                <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+                <p className='md:text-2xl text-xl font-bold text-gray-500'>Learn the Best Technology with Us. Increase revenue for Desktop Laptop Mobile app platforms.</p>
                 <button className='bg-cyan-700 w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white'>Get Started</button>
             </div>
         </div>
