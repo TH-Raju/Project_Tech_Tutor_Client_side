81afd56a7e3575f0b368c19233babfce759920c0
TH-Raju <rajukhan894200@gmail.com>
2022-11-07 22:23:28 +0600

    firebase connected and router setup

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,19 @@
+import Home from "../components/Home/Home";
+import Main from "../layout/Main";
+
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+
+])
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from './Banner';
+
+const Home = () => {
+    return (
+        <div>
+            Home
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
