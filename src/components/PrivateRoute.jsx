2cbf6b01ec039ac6600256b8b1089d25bec8b5e1
TH-Raju <rajukhan894200@gmail.com>
2022-11-08 21:28:59 +0600

    Private route setup

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../context/AuthProvider';
+
+const PrivateRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
+    const location = useLocation();
+
+
+
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace></Navigate>
+    }
+    return children;
+};
+
+export default PrivateRoute; 
