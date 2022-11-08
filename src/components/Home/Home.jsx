c0ab7943b6dc1cfe1f8deec0d786a4b2c0f49dbc
TH-Raju <rajukhan894200@gmail.com>
2022-11-08 21:49:38 +0600

    home section debugging

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Services from '../Services/Services';
 import Banner from './Banner';
 import Summary from './Summary';
 
 const Home = () => {
     return (
-        <div>
+        <div className='bg-gray-800'>
             <Banner></Banner>
             <Services></Services>
+            <div className='text-center'>
+                <Link to='/services'> <button className='bg-cyan-700 w-[200px] text-center rounded-md font-medium my-6 mx-auto py-3 text-white'>See All</button></Link>
+            </div>
             <Summary></Summary>
         </div>
     );
