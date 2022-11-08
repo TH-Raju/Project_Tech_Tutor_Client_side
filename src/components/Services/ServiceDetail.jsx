19d4f839a38181bc0b4ff62040c86d6ab64c96c5
TH-Raju <rajukhan894200@gmail.com>
2022-11-08 23:21:47 +0600

    service detail page review input added

--- src/components/Services/ServiceDetail.jsx
+++ src/components/Services/ServiceDetail.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
 
 const ServiceDetail = () => {
     const { img, price, title, body } = useLoaderData();
+    const { user } = useContext(AuthContext);
     return (
-        <div className='bg-gray-800'>
+        <div className='bg-gray-800 p-6'>
 
             <section>
-                <div className="container max-w-6xl p-6 space-y-6 sm:space-y-12">
+                <div className="container max-w-6xl space-y-6 sm:space-y-12">
                     <div className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-800">
                         <img src={img} alt="Website Design System" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
                         <div className="p-6 space-y-2 lg:col-span-5">
@@ -23,6 +25,17 @@ const ServiceDetail = () => {
                 </div>
             </section>
 
+            {/* Review Section */}
+            <div>
+                <h3 className="text-2xl text-white font-semibold sm:text-4xl e group-focus:underline pb-6 underline">Reviews</h3>
+                <div className="mb-6">
+                    <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Give a Review</label>
+                    <input type="text" id="text" name='feedback' className="bg-gray-50 border border-gray-300 text-gray-900 text-lg h-16 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your Feedback" required />
+                    <div className='text-end'>
+                        <button className='bg-cyan-700 w-[200px] text-center rounded-md font-medium my-6 mx-auto py-3 text-white'>Post</button>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
