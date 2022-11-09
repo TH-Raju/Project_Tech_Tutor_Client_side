41e237b04411909e5b6a97054e065c58e65cfc9d
TH-Raju <rajukhan894200@gmail.com>
2022-11-10 00:11:24 +0600

    Section Debugging

--- src/components/Home/Summary.jsx
+++ src/components/Home/Summary.jsx
@@ -11,19 +11,19 @@ const Summary = () => {
                         <p className="text-sm sm:text-base">Clients</p>
                     </div>
                     <div className="flex flex-col justify-start m-2 lg:m-6">
-                        <p className="text-4xl font-bold leading-none lg:text-6xl">89K</p>
+                        <p className="text-4xl font-bold leading-none lg:text-6xl">30K</p>
                         <p className="text-sm sm:text-base">Followers on social media</p>
                     </div>
                     <div className="flex flex-col justify-start m-2 lg:m-6">
-                        <p className="text-4xl font-bold leading-none lg:text-6xl">6</p>
-                        <p className="text-sm sm:text-base">Published book</p>
+                        <p className="text-4xl font-bold leading-none lg:text-6xl">20</p>
+                        <p className="text-sm sm:text-base">Published site</p>
                     </div>
                     <div className="flex flex-col justify-start m-2 lg:m-6">
-                        <p className="text-4xl font-bold leading-none lg:text-6xl">8</p>
-                        <p className="text-sm sm:text-base">TECH talks</p>
+                        <p className="text-4xl font-bold leading-none lg:text-6xl">5+</p>
+                        <p className="text-sm sm:text-base">TECH Course</p>
                     </div>
                     <div className="flex flex-col justify-start m-2 lg:m-6">
-                        <p className="text-4xl font-bold leading-none lg:text-6xl">10</p>
+                        <p className="text-4xl font-bold leading-none lg:text-6xl">4</p>
                         <p className="text-sm sm:text-base">Years of experience</p>
                     </div>
                     <div className="flex flex-col justify-start m-2 lg:m-6">
--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -13,10 +13,10 @@ const Service = () => {
         <div>
 
             <div className='text-center'>
-                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
+                <h1 className='md:text-7xl sm:text-6xl text-4xl text-white font-bold md:py-6'>
                     My Services
                 </h1>
-                <p className='md:text-2xl text-xl font-bold text-gray-500'>Here are Some course that i teach people to increase their knowledge. and become self-defendent</p>
+                <p className='md:text-2xl text-xl font-bold text-gray-400'>Here are Some course that i teach people to increase their knowledge. and become self-defendent</p>
             </div>
             <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10'>
                 {
